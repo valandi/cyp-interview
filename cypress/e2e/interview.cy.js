a63f7e4d407010f0ad464f4484b7399e70662e93
Valandi <valandi.allulis@gmail.com>
2024-10-29 12:55:37 -0400

    interview

---
+++ cypress/e2e/interview.cy.js
@@ -0,0 +1,31 @@
+describe('Applitools Interview', () => {
+    beforeEach(() => {
+
+    })
+
+    // Write a test that satisfies the following workflow:
+    //    1. Navigate to https://sandbox.applitools.com/e-commerce
+    //    2. Open the page for "Small Succulent Planter Pot"
+    //    3. Choose "gold" for "Select Color" Dropdown
+    //    4. Increment count to 3. 
+    //    5. Click "Add to cart"
+    //    6. Navigate to cart (cart icon in the top right corner)
+    //    7. Click "Checkout"
+    //    8. Write an assertion to ensure that the total price of the cart is $9.69. 
+    it('Applitools e-commerce test', () => {
+        
+    })
+
+    // Part 2
+    // Write a test that satisfies the following workflow:
+    // 1. Navigate to https://the-internet.herokuapp.com/hovers
+    // 2. Hover over the first image
+    // 3. Write an assertion to ensure that the text "name: user1" is visible
+    it('Hover Test', () => {
+       
+    })
+
+    afterEach(() => {
+    })
+
+});
